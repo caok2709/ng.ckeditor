0e6c5d8848fc6f41181122af10d77a75a8dd3c58
Kuan Cao <davidck2012@gwmail.gwu.edu>
2016-04-07 11:48:04 -0400

    4/7/2016 update

--- angularjs/plugins/ng-ckeditor/ng-ckeditor.js
+++ angularjs/plugins/ng-ckeditor/ng-ckeditor.js
@@ -10,32 +10,14 @@
 	"use strict";
 	(angular.module('ng.ckeditor', [ 'ng' ])).directive('ngCkeditor', function() {
 
-		CKEDITOR.on('instanceCreated', function(event) {
-			var editor = event.editor, element = editor.element;
-			if (element&&  element.getAttribute('class') && element.getAttribute('class').indexOf('simpleEditor')!=-1 ) {
-				editor.on('configLoaded', function() {
-					//editor.config.removePlugins = 'colorbutton,find,flash,font, forms,iframe,image,newpage,removeformat, smiley,specialchar,stylescombo,templates';
-					//editor.removeButtons = 'About';
-					editor.config.toolbarGroups = [
-						{name : 'basicstyles',	groups : [ 'basicstyles','cleanup']},
-						{name : 'paragraph',	groups: [ 'list', 'indent' ] },
-						{name : 'styles'},
-						{name : 'clipboard',	groups : [ 'clipboard','undo' ]},
-						{name : 'links'},
-						{name : 'insert'}
-					]
-					editor.config.removePlugins ='horizontalrule,specialchar';
-					editor.config.removeButtons='Strike,Subscript,Superscript,Anchor,Format';
-
-					if(element.getAttribute('disabled')==='true'){
-						editor.config.readOnly = true;
-					}
-				});
-			}
-		});
-
 		var container = function(scope, element, attrs, ngModel) {
+			if(ngModel === undefined){
+				console.error("ngCkeditor need attribute ngModel. Without it, changing the Note value won't set From.$diray to true!");
+			}
+
+			scope.ckeditor =  CKEDITOR;
 			element[0].innerHTML = '<div id="' + attrs.bind + '"></div>';
+
 			var config = {
 				removeButtons : (attrs.removeButtons != undefined) ? 'About,' + attrs.removeButtons : 'About'
 			};
@@ -61,18 +43,54 @@
 				config.resize_enabled=(attrs.resizeEnabled =="false")?false:true;
 			}
 
-			if(ngModel != undefined){
-				var ck = CKEDITOR.replace(element[0],config);
-				ck.on('change', function() {
-					ngModel.$setViewValue(ck.getData());
-				});
-				ngModel.$render = function(value) {
-					ck.setData(ngModel.$modelValue);
-				};
-			}
-			else{
-				console.error("ngCkeditor need attribute ngModel. Without it, changing the Note value won't set From.$diray to true!");
-			}
+			scope.$watch('ckeditor.on',function(){
+				if(CKEDITOR.on){
+					CKEDITOR.on('instanceCreated', function(event) {
+						var editor = event.editor, element = editor.element;
+						editor.on('configLoaded', function() {
+							//editor.config.removePlugins = 'colorbutton,find,flash,font, forms,iframe,image,newpage,removeformat, smiley,specialchar,stylescombo,templates';
+							//editor.removeButtons = 'About';
+							editor.config.toolbarGroups = [
+								{name : 'basicstyles',	groups : [ 'basicstyles','cleanup']},
+								{name : 'paragraph',	groups: [ 'list', 'indent' ] },
+								{name : 'styles'},
+								{name : 'clipboard',	groups : [ 'clipboard','undo' ]},
+								{name : 'links'},
+								{name : 'insert'}
+							]
+							editor.config.removePlugins ='horizontalrule,specialchar';
+							editor.config.removeButtons='Strike,Subscript,Superscript,Anchor,Format';
+
+							if(element.getAttribute('disabled')==='true'){
+								editor.config.readOnly = true;
+							}
+						});
+						if(ngModel){
+							//Initial rest ckeditor instance
+							editor.setData(ngModel.$modelValue);
+						}
+					});
+				}
+			})
+
+			scope.$watch('ckeditor.replace',function(){
+				if(CKEDITOR.replace){
+					var ck =CKEDITOR.replace(attrs.bind,config);
+					//Initial first ckeditor instance
+					if(ngModel){
+						ngModel.$render = function(value) {
+							ck.setData(ngModel.$modelValue);
+						};
+					}
+
+					ck.on('change', function() {
+						if(ngModel){
+							ngModel.$setViewValue(ck.getData());
+						}
+					});
+				}
+			});
+
 		};
 		return {
 			restrict : 'E',
